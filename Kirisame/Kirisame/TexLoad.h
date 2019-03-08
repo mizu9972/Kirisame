a03a31631706131c234afd60d5d7115043543676
mizu9972 <oisiimizu9972@icloud.com>
2019-03-09 02:53:27 +0900

    文字化け修正

--- Kirisame/Kirisame/TexLoad.h
+++ Kirisame/Kirisame/TexLoad.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 
 #include "2dpolygon.h"
 
@@ -9,6 +9,7 @@ class _TexOp {
 private:
 public:
 	static LPDIRECT3DTEXTURE9* PlayerTex;//プレイヤー
+	static LPDIRECT3DTEXTURE9* PlayerLightTex;//プレイヤーの光沢
 	static LPDIRECT3DTEXTURE9* BrownBearTex;//敵キャラクター
 	static LPDIRECT3DTEXTURE9* EnemyAttackTex;//敵キャラクター攻撃
 	static LPDIRECT3DTEXTURE9* EnemySunabokoriTex;//敵キャラ砂ぼこり
